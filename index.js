212d21e6d120eea772b4a573eb295cacc488fa90
Alice <freshjs.ofte@gmail.com>
2022-02-15 21:02:25 +0200

    Read-n-write task resolve

--- index.js
+++ index.js
@@ -6,6 +6,13 @@
 
 const fs = require('fs').promises;
 
-fs.readFile('./text.txt', 'utf8').then((data)=>{
-    console.log(data);
+fs.readFile('./text.txt', 'utf8').then((content)=>{
+    const newContentToWrite = `Old content: ${content}, new content: Hello world`;
+    fs.writeFile('./newFile.txt', newContentToWrite, 'utf8');
 })
+
+/*
+1. Создать файл (вручную) и написать туда какой-то текст
+2. Прочитать файл и на основании его содержимого + новой строки (lorem10) создать новый файл и записать туда этот текст
+
+*/
