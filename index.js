29959b29da20955f4926bc23312ec9532b25b2f4
Alice <freshjs.ofte@gmail.com>
2022-02-15 20:36:01 +0200

    FileSystem

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const {MyMath} = require('./MyMath');
-const {Component} = require('./Component');
+// const fs = require('fs/promises'); - вариант 1
+// const fs = require('fs').promises; - вариант 2
 
+//const fs = require('fs');
+//fs.promises; // - вариант 3
 
-/* 
-How require() works:
-Resolving -> Loading -> Wrapping -> Evaluation -> Caching
+const fs = require('fs').promises;
 
-1. Core module
-2. File
-    2.1 *.js
-    2.2 *.json
-3. Directory
-    3.1 package.json -> "main"
-    3.2 index.js | index.json
-4. node_modules
-5. throw new Error()
-
-*/
-
-const c = new Component();
-c.render();
-
-const res = MyMath.sum(3,3);
-console.log('index.js');
+fs.readFile('./text.txt', 'utf8').then((data)=>{
+    console.log(data);
+})
