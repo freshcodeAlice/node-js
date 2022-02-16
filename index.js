31e70a941239016c66aec29769770976a7c547c8
Alice <freshjs.ofte@gmail.com>
2022-02-16 18:27:03 +0200

    Lodash add

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// const fs = require('fs/promises'); - вариант 1
-// const fs = require('fs').promises; - вариант 2
+const _ = require('lodash');
 
-//const fs = require('fs');
-//fs.promises; // - вариант 3
+const users = [
+    { 'user': 'barney', 'age': 36, 'active': true },
+    { 'user': 'fred',   'age': 40, 'active': false }
+  ];
 
-const fs = require('fs').promises;
-
-fs.readFile('./text.txt', 'utf8').then((content)=>{
-    const newContentToWrite = `Old content: ${content}, new content: Hello world`;
-    fs.writeFile('./newFile.txt', newContentToWrite, 'utf8');
-})
-
-/*
-1. Создать файл (вручную) и написать туда какой-то текст
-2. Прочитать файл и на основании его содержимого + новой строки (lorem10) создать новый файл и записать туда этот текст
-
-*/
+  console.log(_.filter(users, 'active'));
